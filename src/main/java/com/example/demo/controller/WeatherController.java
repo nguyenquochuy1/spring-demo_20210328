b5cc02dbddea47f10c853bcbcadbdd96b9bea28f
nquochuy1995 <n.q.huy@office-fa.com>
2021-08-01 00:12:42 +0900

    add some thing not important

--- src/main/java/com/example/demo/controller/WeatherController.java
+++ src/main/java/com/example/demo/controller/WeatherController.java
@@ -7,20 +7,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class WeatherController {
 
     @Autowired
     WeatherService weatherService;
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @GetMapping("/weather")
     public String demo(Model model) throws Exception {
         @NonNull
         List<Weather> weatherDataList = weatherService.findAllWeatherData();
         model.addAttribute("weatherDataList", weatherDataList);
+//        List<Map<String, Object>> list = jdbcTemplate.queryForList("select  * from weather");
+//        model.addAttribute("weatherDataList", list);
         return "weather";
     }
 }
